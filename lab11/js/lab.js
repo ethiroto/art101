4d00819ee9bbda8a61b83d03398589066ee97ef6
ethiroto <squidwhich@gmail.com>
2023-05-17 16:17:51 -0700

    lab 11!

---
+++ lab11/js/lab.js
@@ -0,0 +1,43 @@
+// index.js - Libraries and jQuery
+// Author: Ethan Christian and Aaron Rodriguez
+// Date: 5/17/2023
+
+
+//finding the elements
+var challengeEl=$('#challenge');
+var problemEl=$('#problem');
+var outputEl=$('#output')
+var resultsEl=$('#results')
+
+//creating the button elements
+var cButton= $('<button> Press me!! </button>');
+var pButton= $('<button> Press me! </button>');
+var rButton= $('<button> Press me! </button>');
+
+//Adding the buttons to the elements
+challengeEl.append(cButton);
+problemEl.append(pButton);
+resultsEl.append(rButton);
+
+//creating a function that takes in where you want to toggle the class
+function specialActivate(id){
+    id.toggleClass('special');
+}
+
+//Assigning the event listners to
+cButton.on('click', function() {
+    specialActivate(challengeEl);
+});
+
+pButton.on('click', function() {
+    specialActivate(problemEl);
+});
+
+rButton.on('click', function() {
+    specialActivate(resultsEl);
+});
+
+
+
+
+
